a4d787f957a8d5c50de02cf672b8fc6d8711789e
venkatanatha av <venkatanathaav@gmail.com>
2025-12-10 16:09:24 +0530

    file handelling

--- main.cpp
+++ main.cpp
@@ -1,42 +1,44 @@
 #include <iostream>
 #include <string>
-#include<vector>
+// #include<vector>
 
+constexpr int maxbuffer=1024;
 using namespace std;
-
-struct corners{
-    float a,b,c,d;
-};
-
-
-ostream& operator<<(ostream& stream,const corners& corners){
-    stream << corners.a<<""<<corners.b<<""<<corners.c<<""<<corners.d<<endl;
-}
 
 
 int main(){
 
-    vector<int> inty;
-    inty.push_back(2);
-    inty.push_back(3);
+    static const char * originalfile="originalfile.txt";
+    const char* informatin="Caroline, don't go into the light ";
 
-    for (auto i=inty.begin();i!=inty.end();++i){
-        cout<< *i<<endl;
+    // std::FILE * fh=fopen(originalfile,"a");
+
+    // // fclose(fh);
+
+    // cout<<"file closed"<<endl;
+    
+    
+    // for (int i=0;i<50;++i){
+    //     fputs(informatin,fh);
+    // }
+    // fclose(fh);
+
+    char buff[maxbuffer];
+
+    FILE  * fh =fopen(originalfile,"r");
+    while (fgets(buff,maxbuffer,fh)){
+        fputs(buff,stdout);
+        
     }
-
-    for (auto i=inty.begin();i!=inty.end();++i){
-        cout<< *i<<endl;
-    }
-
-    vector <corners> corners;
-
-    corners.push_back({1,2,3,4});
-    corners.push_back({5,6,7,8});
-
-    for (int i=0;i< corners.size();++i){
-        cout<<corners[i]<<endl;     
-    }
+    fclose(fh);
 
 
+
+    // remove(originalfile);
+
+
+    return 0;
 }
+
+
 
