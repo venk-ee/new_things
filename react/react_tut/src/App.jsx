53bfa732683f24769c5e9a2fc38fafe45f3271e7
venkatanatha <venkatanathaav@gmail.com>
2026-05-19 11:48:15 +0530

    feat: implement AuthContext and provide user state to the application components,useEffect,addEventListener

--- react/react_tut/src/App.jsx
+++ react/react_tut/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import {Link, Route, Routes} from 'react-router-dom'
+import AuthContext from './AuthContext'
 
 
 function Home() {
+  const {user} = useContext(AuthContext)
   return (
     <div>
       <h1>Home</h1>
@@ -11,18 +13,28 @@ function Home() {
   )
 }
 
+window.addEventListener('storage', () => {
+  console.log('Storage changed')
+})
+
+window.addEventListener('mosemove',()=>{
+  console.log('Mouse moved')
+})
 
 function App() {
+  const [user, setUser] = useState({name:"kenny",age:23})
   return (
     <div>
       <nav>
         <Link to="/">Home</Link>
+        
       </nav>
-
+    <AuthContext.Provider value={null} >
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="*" element={<h1>404 Not Found</h1>}/>
       </Routes>
+    </AuthContext.Provider>
     </div>
   )
 
