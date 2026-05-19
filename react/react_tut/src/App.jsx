eb771dcca119840994a2b3ec50ad0bdcfb9be27b
venkatanatha <venkatanathaav@gmail.com>
2026-05-19 11:51:42 +0530

    refactor: remove AuthContext and routing logic in favor of a data fetching effect in App component

--- react/react_tut/src/App.jsx
+++ react/react_tut/src/App.jsx
@@ -1,45 +1,21 @@
 import { useState,useEffect } from 'react'
 import './App.css'
 import {Link, Route, Routes} from 'react-router-dom'
-import AuthContext from './AuthContext'
 
 
-function Home() {
-  const {user} = useContext(AuthContext)
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
-
-window.addEventListener('storage', () => {
-  console.log('Storage changed')
-})
-
-window.addEventListener('mosemove',()=>{
-  console.log('Mouse moved')
-})
 
 function App() {
-  const [user, setUser] = useState({name:"kenny",age:23})
-  return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        
-      </nav>
-    <AuthContext.Provider value={null} >
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="*" element={<h1>404 Not Found</h1>}/>
-      </Routes>
-    </AuthContext.Provider>
-    </div>
-  )
+
+  useEffect(()=>{
+    console.log("use effect");
+    try{
+      fetch("https://jsonplaceholder.typicode.com/posts");
+    }catch (error){
+      console.error("error", error);
+    }
+  },[])
 
 }
-
 
 
 
