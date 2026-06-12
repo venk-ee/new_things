0cd695e3a4fc8d0035418ef5306e4482cf215cb3
venkatanatha <venkatanathaav@gmail.com>
2026-06-12 18:09:28 +0530

    feat: add tutorial 2 script and update DOM playground in index.html

---
+++ js/tut2.js
@@ -0,0 +1,11 @@
+const playground=document.getElementById("playground");
+
+playground.append("lol",'kdskdnsdk',1);
+
+const h1=document.createElement("h1");
+h1.textContent="lol";
+console.log(h1);
+
+playground.append(h1);
+
+h1.remove();
